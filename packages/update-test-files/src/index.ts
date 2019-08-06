aa6153789e8d2fae4508243485079eee489a8f08
Zihan Chen <zihanc@microsoft.com>
2019-08-05 17:39:13 -0700

    Begin converting CodegenSchema.js

--- packages/update-test-files/src/index.ts
+++ packages/update-test-files/src/index.ts
@@ -1,1 +1,20 @@
-console.log('Converting files...');
+import * as fs from 'fs';
+import * as path from 'path';
+
+function convertCodegenSchema(): void {
+  console.log('Converting CodegenSchema.js ...');
+
+  const inputPath = path.join(__dirname, `../../../react-native/packages/react-native-codegen/src/CodegenSchema.js`);
+  const outputPath = path.join(__dirname, `../../RN-TSCodegen/src/CodegenSchema.ts`);
+  const flowSourceCode = fs.readFileSync(inputPath, { encoding: 'utf-8' });
+  const tsSourceCode = flowSourceCode
+    .replace(`$ReadOnly<`, `Readonly<`)
+    .replace(`$ReadOnlyArray<`, `ReadonlyArray<`)
+    .replace(`{|`, `{`)
+    .replace(`{|}`, `}`)
+    .replace(`: ?`, `: null | undefined | `)
+    ;
+  fs.writeFileSync(outputPath, tsSourceCode, { encoding: 'utf-8' });
+}
+
+convertCodegenSchema();
