bcc8658dcb933b9114b48a4e0cefb792f464e39a
Zihan Chen <zihanc@microsoft.com>
2019-08-08 16:57:17 -0700

    ...

--- packages/RN-TSCodegen/src/ComponentEventParser.ts
+++ packages/RN-TSCodegen/src/ComponentEventParser.ts
@@ -1,61 +1,71 @@
 import * as ts from 'typescript';
 import * as cs from './CodegenSchema';
 import { ExportComponentInfo } from './ExportParser';
-import { isNull, WritableObjectType } from './TypeChecker';
+import { isNull, isVoid, WritableObjectType } from './TypeChecker';
+
+function checkEventType(eventType: ts.Type, info: ExportComponentInfo, propDecl: ts.PropertySignature): [boolean, ts.Type, string, string] {
+  if (eventType.isUnion()) {
+    let result: [boolean, ts.Type, string, string];
+    for (const elementType of eventType.types) {
+      if (!isNull(elementType) && !isVoid(elementType)) {
+        const elementResult = checkEventType(elementType, info, propDecl);
+        if (result === undefined) {
+          result = elementResult;
+        } else {
+          throw new Error(`Event ${propDecl.name.getText()} in type ${info.typeNode.getText()} should have a correct event type.`);
+        }
+      }
+    }
+    result[0] = true;
+    return result;
+  } else {
+    if (eventType.aliasSymbol === undefined || (eventType.aliasSymbol.name !== 'DirectEventHandler' && eventType.aliasSymbol.name !== 'BubblingEventHandler')) {
+      return undefined;
+    }
+
+    const typeArguments = eventType.aliasTypeArguments;
+    if (typeArguments === undefined || typeArguments.length < 1 || typeArguments.length > 2) {
+      throw new Error(`Event ${propDecl.name.getText()} in type ${info.typeNode.getText()} should have one or two type parameters for ${eventType.aliasSymbol.name}.`);
+    }
+
+    if (typeArguments.length === 2) {
+      const nameArgument = typeArguments[1];
+      if (nameArgument.isStringLiteral()) {
+        return [false, typeArguments[0], eventType.aliasSymbol.name, nameArgument.value];
+      }
+    }
+    return [false, typeArguments[0], eventType.aliasSymbol.name, undefined];
+  }
+}
 
 export function tryParseEvent(info: ExportComponentInfo, propDecl: ts.PropertySignature): cs.EventTypeShape {
-  if (!ts.isTypeReferenceNode(propDecl.type)) {
-    return undefined;
-  }
-
-  let eventTypeName: string;
-  let paperTopLevelNameDeprecated: string;
-
-  if (ts.isIdentifier(propDecl.type.typeName)) {
-    eventTypeName = propDecl.type.typeName.text;
-  } else if (ts.isQualifiedName(propDecl.type.typeName)) {
-    eventTypeName = propDecl.type.typeName.right.text;
-  }
-  if (eventTypeName !== 'DirectEventHandler' && eventTypeName !== 'BubblingEventHandler') {
-    return undefined;
-  }
-
-  const wrongTypeArgumentError = new Error(`Event ${propDecl.name.getText()} in type ${info.typeNode.getText()} should have one or two type parameters for ${eventTypeName}.`);
-  if (propDecl.type.typeArguments === undefined || propDecl.type.typeArguments.length < 1 || propDecl.type.typeArguments.length > 2) {
-    throw wrongTypeArgumentError;
-  }
-  if (propDecl.type.typeArguments.length === 2) {
-    const nameArgument = propDecl.type.typeArguments[1];
-    if (!ts.isStringLiteral(nameArgument)) {
-      throw wrongTypeArgumentError;
-    }
-    paperTopLevelNameDeprecated = nameArgument.text;
-  }
-
   const typeChecker = info.program.getTypeChecker();
-  const eventType = typeChecker.getTypeFromTypeNode(propDecl.type.typeArguments[0]);
+  const eventTypeTuple = checkEventType(typeChecker.getTypeFromTypeNode(propDecl.type), info, propDecl);
+  if (eventTypeTuple === undefined) {
+    return undefined;
+  }
+  const [optional, eventType, eventTypeName, paperTopLevelNameDeprecated] = eventTypeTuple;
+
+  const eventProperties: WritableObjectType<cs.ObjectPropertyType>[] = [];
+  if (!isNull(eventType)) {
+    // nothing;
+  }
 
   const result: WritableObjectType<cs.EventTypeShape> = {
     name: propDecl.name.getText(),
     bubblingType: eventTypeName === 'DirectEventHandler' ? 'direct' : 'bubble',
-    optional: propDecl.questionToken !== undefined,
+    optional: (propDecl.questionToken !== undefined) || optional,
     typeAnnotation: {
-      type: 'EventTypeAnnotation'
+      type: 'EventTypeAnnotation',
+      argument: {
+        type: 'ObjectTypeAnnotation',
+        properties: eventProperties
+      }
     }
   };
 
-  if (paperTopLevelNameDeprecated === undefined) {
+  if (paperTopLevelNameDeprecated !== undefined) {
     result.paperTopLevelNameDeprecated = paperTopLevelNameDeprecated;
   }
-
-  if (isNull(eventType)) {
-    return result;
-  } else {
-    const eventProperties: WritableObjectType<cs.ObjectPropertyType>[] = [];
-    result.typeAnnotation.argument = {
-      type: 'ObjectTypeAnnotation',
-      properties: eventProperties
-    };
-    return result;
-  }
+  return result;
 }
