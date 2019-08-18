19d61c9acb588ed46d1165701e2354977c42fc3f
Zihan Chen (MSFT) <zihanc@microsoft.com>
2019-08-18 15:49:11 -0700

    More test cases

--- packages/ts-parsec/test/TestLexer.ts
+++ packages/ts-parsec/test/TestLexer.ts
@@ -52,3 +52,74 @@ test(`Test number with commas (multiple token)`, () => {
 
     assert.equal(token, undefined);
 });
+
+test(`Test number with commas (discard commas)`, () => {
+    enum TokenKind {
+        Number,
+        Comma,
+    }
+
+    const lexer = buildLexer<TokenKind>([
+        [false, /^\d+/g, TokenKind.Number],
+        [true, /^,/g, TokenKind.Comma]
+    ]);
+
+    let token = lexer.parse(`123,456,789`);
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Number);
+    assert.equal(token.text, '123');
+    token = token.next;
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Number);
+    assert.equal(token.text, '456');
+    token = token.next;
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Number);
+    assert.equal(token.text, '789');
+    token = token.next;
+
+    assert.equal(token, undefined);
+});
+
+test(`Test identifiers and numbers with discardable commas and spaces`, () => {
+    enum TokenKind {
+        Number,
+        Identifier,
+        Comma,
+        Space,
+    }
+
+    const lexer = buildLexer<TokenKind>([
+        [false, /^\d+/g, TokenKind.Number],
+        [false, /^[a-zA-Z]\w*/g, TokenKind.Identifier],
+        [true, /^,/g, TokenKind.Comma],
+        [true, /^\s+/g, TokenKind.Comma]
+    ]);
+
+    let token = lexer.parse(`123, abc, 456, def, `);
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Number);
+    assert.equal(token.text, '123');
+    token = token.next;
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Identifier);
+    assert.equal(token.text, 'abc');
+    token = token.next;
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Number);
+    assert.equal(token.text, '456');
+    token = token.next;
+
+    assert.notEqual(token, undefined);
+    assert.equal(token.kind, TokenKind.Identifier);
+    assert.equal(token.text, 'def');
+    token = token.next;
+
+    assert.equal(token, undefined);
+});
