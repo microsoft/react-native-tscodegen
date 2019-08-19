4f14d02b80034594f3d52666fff8502e54d2f244
Zihan Chen (MSFT) <zihanc@microsoft.com>
2019-08-18 19:18:57 -0700

    Update ParserInterface.ts

--- packages/ts-parsec/src/Parsers/ParserInterface.ts
+++ packages/ts-parsec/src/Parsers/ParserInterface.ts
@@ -22,3 +22,18 @@ export function succeeded<TKind, TResult>(r: ParseResult<TKind, TResult>[] | Par
 export function failed<TKind, TResult>(r: ParseResult<TKind, TResult>[] | ParseError): r is ParseError {
     return !(r instanceof Array);
 }
+
+export function betterError(e1: ParseError | undefined, e2: ParseError | undefined): ParseError | undefined {
+    if (e1 === undefined) { return e2; }
+    if (e2 === undefined) { return e1; }
+    if (e1.pos === undefined) { return e1; }
+    if (e2.pos === undefined) { return e2; }
+
+    if (e1.pos.index < e2.pos.index) {
+        return e2;
+    } else if (e1.pos.index > e2.pos.index) {
+        return e1;
+    } else {
+        return e1;
+    }
+}
