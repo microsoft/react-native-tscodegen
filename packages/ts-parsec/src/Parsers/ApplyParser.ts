290627792f6d59e8ba71a663375335ae0af455b4
Zihan Chen (MSFT) <zihanc@microsoft.com>
2019-08-18 19:07:03 -0700

    ...

--- packages/ts-parsec/src/Parsers/ApplyParser.ts
+++ packages/ts-parsec/src/Parsers/ApplyParser.ts
@@ -1,15 +1,20 @@
 import { Token } from '../Lexer';
-import { ParseError, Parser, ParseResult } from './ParserInterface';
+import { ParseError, Parser, ParseResult, succeeded } from './ParserInterface';
 
 export function apply<TKind, TFrom, TTo>(p: Parser<TKind, TFrom>, callback: (value: TFrom) => TTo): Parser<TKind, TTo> {
     return {
         parse(token: Token<TKind>): ParseResult<TKind, TTo>[] | ParseError {
-            return p.parse(token).map((value: ParseResult<TKind, TFrom>) => {
-                return {
-                    nextToken: value.nextToken,
-                    result: callback(value.result)
-                };
-            });
+            const result = p.parse(token);
+            if (succeeded(result)) {
+                return result.map((value: ParseResult<TKind, TFrom>) => {
+                    return {
+                        nextToken: value.nextToken,
+                        result: callback(value.result)
+                    };
+                });
+            } else {
+                return result;
+            }
         }
     };
 }
