ce68829e338a33f52b14c64069a0a9831ee9aa19
Zihan Chen <zihanc@microsoft.com>
2019-08-20 14:30:46 -0700

    Update Init.bat

--- Init.bat
+++ Init.bat
@@ -1,36 +1,24 @@
 pushd packages
 pushd tslint-shared
 call npm link
-popd
-pushd ts-parsec
-call npm link
+call npm install
 popd
 pushd ts-parsec
 call npm link tslint-shared
+call npm link
+call npm install
 popd
 pushd update-test-files
 call npm link tslint-shared
 call npm link ts-parsec
-popd
-pushd RN-TSCodegen
-call npm link tslint-shared
-popd
-pushd RN-TSCodegen-Test
-call npm link tslint-shared
-popd
-pushd tslint-shared
-call npm install
-popd
-pushd ts-parsec
-call npm install
-popd
-pushd update-test-files
 call npm install
 popd
 pushd RN-TSCodegen
+call npm link tslint-shared
 call npm install
 popd
 pushd RN-TSCodegen-Test
+call npm link tslint-shared
 call npm install
 popd
 popd
