2011c0f96e3d39c9f62714236c6aeb758041496b
Zihan Chen (MSFT) <zihanc@microsoft.com>
2019-08-28 16:22:22 -0700

    Ensure converting e2e test cases to CodegenSchema without exception

--- packages/RN-TSCodegen/test/TestSuccessCases.ts
+++ packages/RN-TSCodegen/test/TestSuccessCases.ts
@@ -12,6 +12,8 @@ interface TestCaseCategory {
 interface TestCaseIndex {
     components: TestCaseCategory;
     modules: TestCaseCategory;
+    e2e_components: TestCaseCategory;
+    e2e_modules: TestCaseCategory;
 }
 
 const testCaseIndex = <TestCaseIndex>JSON.parse(readFileSync(
@@ -36,5 +38,19 @@ testCaseIndex.modules.success.forEach((key: string) => {
         const schema = typeScriptToCodeSchema(inputFile, 'NativeSampleTurboModule', 'SampleTurboModule');
         const snapshot = JSON.parse(readFileSync(snapshotFile, { encoding: 'utf-8' }));
         assert.deepEqual(schema, snapshot);
+    });
+});
+
+testCaseIndex.e2e_components.success.forEach((key: string) => {
+    test(`e2e components codegen: ${key}`, () => {
+        const inputFile = path.join(__dirname, `../../../RN-TSCodegen-Test/src/inputs/e2e_components/${key}.ts`);
+        typeScriptToCodeSchema(inputFile, 'E2EModule');
+    });
+});
+
+testCaseIndex.e2e_modules.success.forEach((key: string) => {
+    test(`e2e modules codegen: ${key}`, () => {
+        const inputFile = path.join(__dirname, `../../../RN-TSCodegen-Test/src/inputs/e2e_modules/${key}.ts`);
+        typeScriptToCodeSchema(inputFile, 'E2ENativeModule');
     });
 });
