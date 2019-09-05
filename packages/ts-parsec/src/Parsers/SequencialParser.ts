a170b24fb8ed205401791d7674dec22edc4f219b
Zihan Chen <zihanc@microsoft.com>
2019-09-05 14:46:17 -0700

    Auto generate overloading candidates for seq and alt

--- packages/ts-parsec/src/Parsers/SequencialParser.ts
+++ packages/ts-parsec/src/Parsers/SequencialParser.ts
@@ -4,6 +4,8 @@
 import { Token } from '../Lexer';
 import { betterError, ParseError, Parser, ParseResult, ParserOutput, resultOrError } from './ParserInterface';
 
+// CodegenOverloadings:Begin
+
 export function seq<TKind, T1, T2>(
     p1: Parser<TKind, T1>,
     p2: Parser<TKind, T2>
@@ -113,6 +115,78 @@ export function seq<TKind, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(
     p11: Parser<TKind, T11>,
     p12: Parser<TKind, T12>
 ): Parser<TKind, [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12]>;
+
+export function seq<TKind, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(
+    p1: Parser<TKind, T1>,
+    p2: Parser<TKind, T2>,
+    p3: Parser<TKind, T3>,
+    p4: Parser<TKind, T4>,
+    p5: Parser<TKind, T5>,
+    p6: Parser<TKind, T6>,
+    p7: Parser<TKind, T7>,
+    p8: Parser<TKind, T8>,
+    p9: Parser<TKind, T9>,
+    p10: Parser<TKind, T10>,
+    p11: Parser<TKind, T11>,
+    p12: Parser<TKind, T12>,
+    p13: Parser<TKind, T13>
+): Parser<TKind, [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13]>;
+
+export function seq<TKind, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(
+    p1: Parser<TKind, T1>,
+    p2: Parser<TKind, T2>,
+    p3: Parser<TKind, T3>,
+    p4: Parser<TKind, T4>,
+    p5: Parser<TKind, T5>,
+    p6: Parser<TKind, T6>,
+    p7: Parser<TKind, T7>,
+    p8: Parser<TKind, T8>,
+    p9: Parser<TKind, T9>,
+    p10: Parser<TKind, T10>,
+    p11: Parser<TKind, T11>,
+    p12: Parser<TKind, T12>,
+    p13: Parser<TKind, T13>,
+    p14: Parser<TKind, T14>
+): Parser<TKind, [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14]>;
+
+export function seq<TKind, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(
+    p1: Parser<TKind, T1>,
+    p2: Parser<TKind, T2>,
+    p3: Parser<TKind, T3>,
+    p4: Parser<TKind, T4>,
+    p5: Parser<TKind, T5>,
+    p6: Parser<TKind, T6>,
+    p7: Parser<TKind, T7>,
+    p8: Parser<TKind, T8>,
+    p9: Parser<TKind, T9>,
+    p10: Parser<TKind, T10>,
+    p11: Parser<TKind, T11>,
+    p12: Parser<TKind, T12>,
+    p13: Parser<TKind, T13>,
+    p14: Parser<TKind, T14>,
+    p15: Parser<TKind, T15>
+): Parser<TKind, [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15]>;
+
+export function seq<TKind, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16>(
+    p1: Parser<TKind, T1>,
+    p2: Parser<TKind, T2>,
+    p3: Parser<TKind, T3>,
+    p4: Parser<TKind, T4>,
+    p5: Parser<TKind, T5>,
+    p6: Parser<TKind, T6>,
+    p7: Parser<TKind, T7>,
+    p8: Parser<TKind, T8>,
+    p9: Parser<TKind, T9>,
+    p10: Parser<TKind, T10>,
+    p11: Parser<TKind, T11>,
+    p12: Parser<TKind, T12>,
+    p13: Parser<TKind, T13>,
+    p14: Parser<TKind, T14>,
+    p15: Parser<TKind, T15>,
+    p16: Parser<TKind, T16>
+): Parser<TKind, [T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16]>;
+
+// CodegenOverloadings:End
 
 export function seq(...ps: Parser<void, {}>[]): Parser<void, {}> {
     return {
