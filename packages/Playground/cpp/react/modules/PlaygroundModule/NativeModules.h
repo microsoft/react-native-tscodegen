480cf2d5a80b9f5df063f15c86785ade4fd60a1c
Zihan Chen (MSFT) <zihanc@microsoft.com>
2019-10-14 17:10:05 -0700

    ...

---
+++ packages/Playground/cpp/react/modules/PlaygroundModule/NativeModules.h
@@ -0,0 +1,26 @@
+
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <ReactCommon/TurboModule.h>
+
+namespace facebook {
+namespace react {
+
+class JSI_EXPORT NativeSampleTurboModuleCxxSpecJSI : public TurboModule {
+protected:
+  NativeSampleTurboModuleCxxSpecJSI(std::shared_ptr<JSCallInvoker> jsInvoker);
+
+public:
+virtual jsi::String getHello(jsi::Runtime &rt, const jsi::String &name) = 0;
+
+};
+
+} // namespace react
+} // namespace facebook
