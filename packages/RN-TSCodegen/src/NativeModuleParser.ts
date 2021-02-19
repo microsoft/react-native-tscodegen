b49fdd2b633d70b2e9e2508bd7699ac4009ad94e
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2021-02-18 17:31:05 -0800

    Compiles!

--- packages/RN-TSCodegen/src/NativeModuleParser.ts
+++ packages/RN-TSCodegen/src/NativeModuleParser.ts
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import * as ts from 'typescript';
 import * as cs from './CodegenSchema';
 import { ExportNativeModuleInfo } from './ExportParser';
 import { RNRawType } from './RNRawType';
 import { typeToRNRawType } from './TypeChecker';
 
-function rawTypeToParamType(rawType: RNRawType): cs.FunctionTypeAnnotationParamTypeAnnotation {
+function rawTypeToBaseType(rawType: RNRawType): cs.NativeModuleBaseTypeAnnotation {
     switch (rawType.kind) {
         case 'String': return { type: 'StringTypeAnnotation' };
         case 'Number': return { type: 'NumberTypeAnnotation' };
@@ -16,13 +15,12 @@ function rawTypeToParamType(rawType: RNRawType): cs.FunctionTypeAnnotationParamT
         case 'Double': return { type: 'DoubleTypeAnnotation' };
         case 'Boolean': return { type: 'BooleanTypeAnnotation' };
         case 'js:Object': return { type: 'GenericObjectTypeAnnotation' };
-        case 'Any': return { type: 'AnyTypeAnnotation' };
         case 'Array': {
             if (rawType.elementType.kind === 'Union' || rawType.elementType.kind === 'Tuple') {
                 const result = { type: 'ArrayTypeAnnotation' };
-                return <cs.FunctionTypeAnnotationParamTypeAnnotation>result;
+                return <cs.NativeModuleBaseTypeAnnotation>result;
             } else {
-                return { type: 'ArrayTypeAnnotation', elementType: rawTypeToParamType(rawType.elementType) };
+                return { type: 'ArrayTypeAnnotation', elementType: rawTypeToBaseType(rawType.elementType) };
             }
         }
         case 'Object': return {
@@ -31,24 +29,9 @@ function rawTypeToParamType(rawType: RNRawType): cs.FunctionTypeAnnotationParamT
                 return {
                     optional: param.propertyType.isNullable,
                     name: param.name,
-                    typeAnnotation: rawTypeToParamType(param.propertyType)
+                    typeAnnotation: rawTypeToBaseType(param.propertyType)
                 };
             })
-        };
-        // What happened?
-        // case 'Function': return {
-        //     type: 'FunctionTypeAnnotation',
-        //     params: rawType.parameters.map((param: { name: string; parameterType: RNRawType }) => {
-        //         return {
-        //             nullable: param.parameterType.isNullable,
-        //             name: param.name,
-        //             typeAnnotation: rawTypeToParamType(param.parameterType)
-        //         };
-        //     }),
-        //     returnTypeAnnotation: rawTypeToReturnType(rawType.returnType)
-        // };
-        case 'Function': return {
-            type: 'FunctionTypeAnnotation'
         };
         default:
     }
@@ -60,35 +43,61 @@ function rawTypeToParamType(rawType: RNRawType): cs.FunctionTypeAnnotationParamT
     }
 }
 
-function rawTypeToReturnType(rawType: RNRawType): cs.FunctionTypeAnnotationReturn {
+function rawTypeToParamType(rawType: RNRawType): cs.NativeModuleParamTypeAnnotation {
     switch (rawType.kind) {
-        case 'String': return { type: 'StringTypeAnnotation', nullable: rawType.isNullable };
-        case 'Number': return { type: 'NumberTypeAnnotation', nullable: rawType.isNullable };
-        case 'Int32': return { type: 'Int32TypeAnnotation', nullable: rawType.isNullable };
-        case 'Float': return { type: 'FloatTypeAnnotation', nullable: rawType.isNullable };
-        case 'Double': return { type: 'DoubleTypeAnnotation', nullable: rawType.isNullable };
-        case 'Boolean': return { type: 'BooleanTypeAnnotation', nullable: rawType.isNullable };
-        case 'js:Object': return { type: 'GenericObjectTypeAnnotation', nullable: rawType.isNullable };
-        case 'Void': case 'Null': return { type: 'VoidTypeAnnotation', nullable: rawType.isNullable };
+        case 'Function': return {
+            type: 'FunctionTypeAnnotation',
+            params: rawType.parameters.map((param: { name: string; parameterType: RNRawType }) => {
+                const parameterType = rawTypeToParamType(param.parameterType);
+                if (param.parameterType.isNullable) {
+                    return {
+                        optional: false,
+                        name: param.name,
+                        typeAnnotation: { type: 'NullableTypeAnnotation', typeAnnotation: parameterType }
+                    };
+                } else {
+                    return {
+                        optional: false,
+                        name: param.name,
+                        typeAnnotation: parameterType
+                    };
+                }
+            }),
+            returnTypeAnnotation: rawTypeToReturnType(rawType.returnType)
+        };
+        default:
+            return rawTypeToBaseType(rawType);
+    }
+}
+
+function rawTypeToReturnType(rawType: RNRawType): cs.NativeModuleReturnTypeAnnotation {
+    switch (rawType.kind) {
+        case 'String': return { type: 'StringTypeAnnotation' };
+        case 'Number': return { type: 'NumberTypeAnnotation' };
+        case 'Int32': return { type: 'Int32TypeAnnotation' };
+        case 'Float': return { type: 'FloatTypeAnnotation' };
+        case 'Double': return { type: 'DoubleTypeAnnotation' };
+        case 'Boolean': return { type: 'BooleanTypeAnnotation' };
+        case 'js:Object': return { type: 'GenericObjectTypeAnnotation' };
+        case 'Void': case 'Null': return { type: 'VoidTypeAnnotation' };
         case 'Array': {
             if (rawType.elementType.kind === 'Union' || rawType.elementType.kind === 'Tuple') {
                 const result = { type: 'ArrayTypeAnnotation', nullable: rawType.isNullable };
-                return <cs.FunctionTypeAnnotationReturn>result;
+                return <cs.NativeModuleReturnTypeAnnotation>result;
             } else {
-                return { type: 'ArrayTypeAnnotation', nullable: rawType.isNullable, elementType: rawTypeToParamType(rawType.elementType) };
+                return { type: 'ArrayTypeAnnotation', elementType: rawTypeToBaseType(rawType.elementType) };
             }
         }
         // What happened?
         // case 'js:Promise': return { type: 'GenericPromiseTypeAnnotation', nullable: rawType.isNullable, resolvedType: rawTypeToReturnType(rawType.elementType) };
-        case 'js:Promise': return { type: 'GenericPromiseTypeAnnotation', nullable: rawType.isNullable };
+        case 'js:Promise': return { type: 'PromiseTypeAnnotation' };
         case 'Object': return {
             type: 'ObjectTypeAnnotation',
-            nullable: rawType.isNullable,
             properties: rawType.properties.map((param: { name: string; propertyType: RNRawType }) => {
                 return {
                     optional: param.propertyType.isNullable,
                     name: param.name,
-                    typeAnnotation: rawTypeToParamType(param.propertyType)
+                    typeAnnotation: rawTypeToBaseType(param.propertyType)
                 };
             })
         };
@@ -100,24 +109,6 @@ function rawTypeToReturnType(rawType: RNRawType): cs.FunctionTypeAnnotationRetur
     } else {
         throw new Error(`${rawType.kind} is not supported in native module.`);
     }
-}
-
-function rawTypeToFunctionTypeAnnotation(rawType: RNRawType, propName: string, typeNode: ts.TypeNode): cs.FunctionTypeAnnotation {
-    if (rawType.kind !== 'Function') {
-        throw new Error(`Member ${propName} in a native module type ${typeNode.getText()} is expected to be a function.`);
-    }
-    return {
-        type: 'FunctionTypeAnnotation',
-        params: rawType.parameters.map((param: { name: string; parameterType: RNRawType }) => {
-            return {
-                nullable: param.parameterType.isNullable,
-                name: param.name,
-                typeAnnotation: rawTypeToParamType(param.parameterType)
-            };
-        }),
-        returnTypeAnnotation: rawTypeToReturnType(rawType.returnType),
-        optional: rawType.isNullable
-    };
 }
 
 export function processNativeModule(info: ExportNativeModuleInfo): cs.NativeModuleSchema {
@@ -126,7 +117,7 @@ export function processNativeModule(info: ExportNativeModuleInfo): cs.NativeModu
         throw new Error(`An object type is expected as a native module: ${info.typeNode.getText()}.`);
     }
 
-    const properties: cs.MethodTypeShape[] = [];
+    const properties: cs.NativeModulePropertySchema[] = [];
     for (const prop of rawType.properties) {
         if (prop.name === 'getConstants' &&
             prop.propertyType.isNullable === true &&
@@ -137,11 +128,25 @@ export function processNativeModule(info: ExportNativeModuleInfo): cs.NativeModu
             // this function is from TurboModule
             continue;
         }
+
+        if (prop.propertyType.kind !== 'Function') {
+            throw new Error(`Member ${prop.name} in a native module type ${info.typeNode.getText()} is expected to be a function.`);
+        }
+
         properties.push({
             name: prop.name,
-            typeAnnotation: rawTypeToFunctionTypeAnnotation(prop.propertyType, prop.name, info.typeNode)
+            optional: false,
+            typeAnnotation: <cs.NativeModuleFunctionTypeAnnotation>rawTypeToParamType(prop.propertyType)
         });
     }
 
-    return { properties };
+    const aliases: cs.NativeModuleAliasMap = {};
+    const spec: cs.NativeModuleSpec = { properties };
+    const moduleNames: string[] = [];
+    const excludedPlatforms: cs.PlatformType[] = [];
+    if (excludedPlatforms.length === 0) {
+        return { type: 'NativeModule', aliases, spec, moduleNames };
+    } else {
+        return { type: 'NativeModule', aliases, spec, moduleNames, excludedPlatforms };
+    }
 }
