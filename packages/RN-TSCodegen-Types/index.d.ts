9c46e602be417ccdcb101503e2783625ce78cf62
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-08-25 16:38:26 -0700

    ...

--- packages/RN-TSCodegen-Types/index.d.ts
+++ packages/RN-TSCodegen-Types/index.d.ts
@@ -2,16 +2,31 @@
 // Licensed under the MIT license.
 
 declare module "react-native-tscodegen-types" {
+  import { ColorValue } from 'react-native';
+
   export * from 'react-native';
+  export { default as codegenNativeCommands } from 'react-native/Libraries/Utilities/codegenNativeCommands';
+  export { default as codegenNativeComponent } from 'react-native/Libraries/Utilities/codegenNativeComponent';
+  export * from 'react-native/Libraries/Types/CodegenTypes';
+
+  export { ImagePropertiesSourceOptions as ImageSource } from 'react-native';
+  export type ColorArrayValue = readonly ColorValue[];
+  export interface PointValue {
+    x: number;
+    bottom: number;
+  }
+  export interface EdgeInsetsValue {
+    top: number;
+    left: number;
+    right: number;
+    bottom: number;
+  }
 
   export type ReactNull = undefined | null;
+  export type FlowOptional<T> = ReactNull | T;
   export type NotString = {};
   export type Stringish = string;
   export type RootTag = number;
-  export type UnsafeObject = {};
-  export type NativeComponent<T> = {};
-  export type NativeComponentType<T> = {};
-  type FlowOptional<T> = ReactNull | T;
 }
 
 declare module "react-native-tscodegen-types" {
@@ -23,4 +38,9 @@ declare module "react-native-tscodegen-types" {
     export type Ref<T> = React.Ref<T>;
     export type ElementRef<T> = React.Ref<T>;
   }
+
+  export function codegenNativeComponent<Props extends object>(
+    componentName: string,
+    options?: Options & { deprecatedViewConfigName: string },
+  ): NativeComponentType<Props>;
 }
