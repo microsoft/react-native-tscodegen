01195b89e908b7734e9125de1e0d8daff34bf81d
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-08-25 15:25:12 -0700

    ...

---
+++ packages/RN-TSCodegen-Parser/src/CodegenSchema.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// Automatically generated from react-native/packages/react-native-codegen/src/CodegenSchema.js
+// Targeting react-native 0.70.0-rc.3
+
+export * from 'react-native-tscodegen/CodegenSchema';
--- packages/RN-TSCodegen-Parser/src/index.ts
+++ packages/RN-TSCodegen-Parser/src/index.ts
@@ -128,14 +128,3 @@ export function typeScriptToCodeSchema(fileName: string, moduleName: string, tar
         return result;
     }
 }
-
-export * from './CodegenSchema';
-
-import * as rncodegen from './ExportRNCodegen';
-
-export namespace generator {
-    export import generate = rncodegen.generate;
-    export import Config = rncodegen.Config;
-    export import Generators = rncodegen.Generators;
-    export import Options = rncodegen.Options;
-}
