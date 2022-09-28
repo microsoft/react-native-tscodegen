0094223436225109583a61ebf16b17091186d494
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-09-28 04:23:45 -0700

    Catch up react-native

--- packages/RN-TSCodegen-Test/test/TestComponentSuccessCases-Original.ts
+++ packages/RN-TSCodegen-Test/test/TestComponentSuccessCases-Original.ts
@@ -12,6 +12,15 @@ testCaseIndex.components.success.forEach((key: string) => {
         const inputFile = path.join(__dirname, `../../src/inputs/components_success/${key}.ts`);
         const snapshotFile = path.join(__dirname, `../../src/inputs/components_success/${key}.json`);
         const schema = typeScriptParser.parseString(readFileSync(inputFile, { encoding: 'utf-8' }), 'NativeSampleTurboModule.ts');
+        for (const moduleName of Object.keys(schema.modules)) {
+            const module = schema.modules[moduleName];
+            if (module.type === 'Component') {
+                for (const componentName of Object.keys(module.components)) {
+                    const component = module.components[componentName];
+                    delete (<{ state?: object }>component).state;
+                }
+            }
+        }
         const snapshot = JSON.parse(readFileSync(snapshotFile, { encoding: 'utf-8' }));
         assert.deepStrictEqual(schema, snapshot);
     });
