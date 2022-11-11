5fdcc4510c9bb93d9d11a199bb86adda281ec4b8
Zihan Chen (MSFT) <53799235+ZihanChen-MSFT@users.noreply.github.com>
2022-11-11 00:14:37 -0800

    Compiled!

--- packages/RN-TSCodegen-Parser/src/ComponentParser.ts
+++ packages/RN-TSCodegen-Parser/src/ComponentParser.ts
@@ -7,7 +7,6 @@ import { parseCommands } from './ComponentCommandParser';
 import { checkEventType, parseEvent } from './ComponentEventParser';
 import { parseProperty } from './ComponentPropertyParser';
 import { ExportCommandInfo, ExportComponentInfo, getMembersFromType } from './ExportParser';
-import { WritableObjectType } from './RNRawType';
 
 function importExists(sourceFile: ts.SourceFile, name: string): boolean {
     const result = sourceFile.forEachChild((importNode: ts.Node) => {
@@ -36,7 +35,6 @@ export function processComponent(info: ExportComponentInfo, commandsInfo?: Expor
     const extendsProps: cs.ExtendsPropsShape[] = [];
     const events: cs.EventTypeShape[] = [];
     const props: cs.NamedShape<cs.PropTypeAnnotation>[] = [];
-    let state: cs.NamedShape<cs.StateTypeAnnotation>[] | undefined;
     let commands: cs.NamedShape<cs.CommandTypeAnnotation>[] = [];
 
     commands = parseCommands(commandsInfo);
@@ -66,34 +64,12 @@ export function processComponent(info: ExportComponentInfo, commandsInfo?: Expor
         extendsProps.push({ knownTypeName: 'ReactNativeCoreViewProps', type: 'ReactNativeBuiltInType' });
     }
 
-    if (info.stateNode !== undefined) {
-        state = [];
-        for (const propDecl of info.stateNode.members) {
-            if (propDecl.name !== undefined) {
-                const propertyName = propDecl.name.getText();
-                if (ts.isPropertySignature(propDecl) || ts.isPropertyDeclaration(propDecl)) {
-                    const eventInfo = checkEventType(<ts.TypeNode>propDecl.type, info, propDecl);
-                    if (eventInfo === undefined) {
-                        state.push(parseProperty(info, propDecl));
-                        continue;
-                    }
-                }
-                throw new Error(`Member ${propertyName} in type ${info.stateNode.name.getText()} is expected to be a state property.`);
-            }
-        }
-    }
-
     const shape: cs.ComponentShape = {
         extendsProps,
         events,
         props,
-        state,
         commands
     };
-
-    if (state === undefined) {
-        delete (<WritableObjectType<cs.ComponentShape>>shape).state;
-    }
 
     Object.getOwnPropertyNames(info.options).forEach((key: string) => {
         shape[key] = info.options[key];
